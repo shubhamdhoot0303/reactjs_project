d29f09ad713cb573903b300eb7e47cc48fe40a01
Shubham Maheshwari <110012111+shubhamdhoot0303@users.noreply.github.com>
2022-09-07 12:31:24 +0530

    useFetch custome hook work with api in react

---
+++ useFecth_custome_hook/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Apicall from "./components/Apicall";
+function App() {
+  return (
+    <>
+      <div className="App">
+        <Apicall />
+      </div>
+    </>
+  );
+}
+
+export default App;
